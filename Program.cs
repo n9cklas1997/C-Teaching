176420821b2c14453e4b5d0da3fec1a93b201633
n9cklas1997 <165781290+n9cklas1997@users.noreply.github.com>
2024-04-02 17:24:17 +0200

    Access Modifiers
    Description of the access modifiers public, private and protected.
    - Missing internal and protected internal

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MyProgram
+{
+        class Program
+    {
+        static void Main()
+        {
+            AccessModifier user1 = new AccessModifier("Lars", "123", 169);
+
+            Console.WriteLine(user1.username); // Only username can be accessed 
+
+            user1.Login("123");
+            Console.WriteLine(user1.GetIsAuthenticated());
+
+            Console.ReadKey();
+        }
+    }
+}
