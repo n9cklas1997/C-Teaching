59aa599092f961ddaf5fb1eefead62501f3929d9
n9cklas1997 <165781290+n9cklas1997@users.noreply.github.com>
2024-04-03 15:17:17 +0200

    Explained command-line arguments
    Added string[] args in the Main.
    Gave example of command-line arguments and how that differs from C

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
         class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             AccessModifier user1 = new AccessModifier("Lars", "123", 169);
 
@@ -15,6 +15,13 @@
             Property user2 = new Property();
             user2.Age = 111;
             Console.WriteLine(user2.Age);
+
+
+            // Command-line arguments for example: dotnet run 666 420
+            // NOTE: In C, the first element argv[0] contained the name of the program.
+            // Here args[0] is the first command-line argument, which would be argv[1] in C.
+            Console.WriteLine("First argument is: " + args[0]);
+            Console.WriteLine("Second argument is: " + args[1]);
 
             Console.ReadKey();
         }
