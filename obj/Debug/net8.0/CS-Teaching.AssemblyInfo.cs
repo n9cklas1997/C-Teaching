b61f726f22267685de809a2b386b32abb6092a35
n9cklas1997 <165781290+n9cklas1997@users.noreply.github.com>
2024-04-07 13:34:38 +0200

    StringsAndLists

--- obj/Debug/net8.0/CS-Teaching.AssemblyInfo.cs
+++ obj/Debug/net8.0/CS-Teaching.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS-Teaching")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59aa599092f961ddaf5fb1eefead62501f3929d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54180e7667c6e0aecfde1f0632c9a579d913beea")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS-Teaching")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS-Teaching")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
